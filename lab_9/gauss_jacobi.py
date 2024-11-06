f606d3d18d029d323a52199049e2f576d5472c5d
vishnu <vishnutejas@iisc.ac.in>
2024-11-06 13:54:07 +0530

    gauss elimination updated

--- lab_9/gauss_jacobi.py
+++ lab_9/gauss_jacobi.py
@@ -1,4 +1,16 @@
 import numpy as np
+def gaussian_elimination(A, b):
+    n = len(b)
+    for i in range(n):
+        for j in range(i+1, n):
+            factor = A[j, i] / A[i, i]
+            A[j, i:] -= factor * A[i, i:]
+            b[j] -= factor * b[i]
+    x = np.zeros(n)
+    for i in range(n-1, -1, -1):
+        x[i] = (b[i] - A[i, i+1:] @ x[i+1:]) / A[i, i]
+    return x
+    
 def gauss_jacobi(A, b, x0, tol, max_iter):
     n = len(b)
     x = np.copy(x0)
@@ -10,7 +22,7 @@ def gauss_jacobi(A, b, x0, tol, max_iter):
                 if j != i:
                     x_new[i] -= A[i, j] * x[j]
             x_new[i] /= A[i, i]
-        if np.linalg.norm(x_new - x) < tol:
+        if (np.linalg.norm(x_new - x,ord=np.inf)/np.linalg.norm(x_new,ord=np.inf)) < tol:
             return x_new,k
         x = np.copy(x_new)
 # check if the method converges
@@ -36,10 +48,12 @@ def gauss_seidel(A, b, x0, tol, max_iter):
                 elif j > i:
                     x_new[i] -= A[i, j] * x[j]
             x_new[i] /= A[i, i]
-        if np.linalg.norm(x_new - x) < tol:
+        if (np.linalg.norm(x_new - x,ord=np.inf)/np.linalg.norm(x_new,ord=np.inf)) < tol:
             return x_new,k
         x = np.copy(x_new)
 
-x_seidel,k_seidel = gauss_seidel(A, b, x0, tol, max_iter)
+# x_seidel,k_seidel = gauss_seidel(A, b, x0, tol, max_iter)
 
-print(x_seidel,k_seidel)
+# print(x_seidel,k_seidel)
+
+print(np.linalg.norm([4,3]))
